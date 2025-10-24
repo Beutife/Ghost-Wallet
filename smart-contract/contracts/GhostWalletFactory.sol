d65f97891258ed93cd4203c450101b79c76dee6a
Jenniscott <scottbellojennifer@gmail.com>
2025-10-24 20:45:29 +0100

    MockUSDC

--- smart-contract/contracts/GhostWalletFactory.sol
+++ smart-contract/contracts/GhostWalletFactory.sol
@@ -3,25 +3,37 @@ pragma solidity ^0.8.20;
 
 import "./GhostWallet.sol";
 import "./EntryPoint.sol";
+import "./MockUSDC.sol";
 
 
 contract GhostFactory {
 
      address public immutable entryPoint;
      mapping(address => address[]) private userWallets;
+     MockUSDC public mockUSDC;
 
-    event GhostCreated(address wallet, address owner);
+     event GhostCreated(address indexed wallet, address indexed owner, uint256 amountMinted);
 
-     constructor(address _entryPoint) {
-        require(_entryPoint != address(0), "Invalid EntryPoint");
+      constructor(address _entryPoint, address _mockUSDC) {
         entryPoint = _entryPoint;
-    }
+        mockUSDC = MockUSDC(_mockUSDC);
+        }
+  
 
-    function createGhost(address owner) external payable returns (address) {
+
+
+    function createGhost(address owner, uint256 mintAmount) external payable returns (address) {
+        require(owner != address(0), "Invalid owner address");
+        require(mintAmount > 0, "Mint amount must be > 0");
+
+        // Deploy Ghost Wallet
         GhostWallet wallet = new GhostWallet{value: msg.value}(owner, entryPoint);
         userWallets[owner].push(address(wallet));
 
-        emit GhostCreated(address(wallet), owner);
+        // Mint mock USDC directly into the new Ghost Wallet
+        mockUSDC.mint(address(wallet), mintAmount);
+
+        emit GhostCreated(address(wallet), owner, mintAmount);
         return address(wallet);
     }
 
